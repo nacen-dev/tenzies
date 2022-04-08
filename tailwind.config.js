aa42eac4ccc1aabafbfa4b2c186bf8d37ad4f5e0
nacen.dev <nacen.dev@gmail.com>
2022-04-08 18:10:44 +0800

    Extend tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background_color: "#0B2434",
+        content_color: "#F5F5F5",
+        dice_freeze: "#59E391",
+        textColor: "#2B283A",
+        subTextColor: "#4A4E74",
+        buttonColor: "#5035FF"
+      },
+    },
   },
   plugins: [],
 };
